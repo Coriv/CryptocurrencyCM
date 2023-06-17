f1e4d79dfa42eea861ca4b1faeb1a4f9fbf96702
Sebastian Boron <sebastian.boron8@gmail.com>
2023-06-17 12:47:06 +0200

    Cryptocurrency service init

---
+++ src/test/java/com/microservices/cryptocurrencyapp/CryptocurrencyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.cryptocurrencyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
